96f204d0b526facbbdd8b8ddc86bca2806d129c6
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-03 20:47:34 +0100

    FEAT name and archetype cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -13,7 +13,8 @@ import axios from'axios'
             axios
                 .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
                 .then((res)=>{
-                    console.log(res)
+                    console.log(res.data.data);
+                    this.cards = res.data.data
                 })
         }
     },
@@ -26,8 +27,9 @@ import axios from'axios'
 <template>
    
     <ul>
-        <li>
-            carta
+        <li v-for="card in cards" :key="card.id">
+            <p>{{card.name}}</p>
+            <p>{{card.archetype}}</p>  
         </li>
     </ul>
 </template>
