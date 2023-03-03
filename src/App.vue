2fcc7d665b36d1fd90c1cf26288969233f27d241
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-03 16:39:08 +0100

    FEAT api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script>
   import Header from './components/Header.vue';
+  import Main from './components/Main.vue';
+
+
   export default{
     components:{
-      Header
+      Header,
+      Main
     }
-
   }
 </script>
 
 <template>
   <Header></Header>
+  <Main></Main>
 </template>
 
 <style lang="scss">
---
+++ src/components/Main.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from'axios'
+
+  export default{
+
+    data(){
+        return{
+            cards:[]
+        }
+    },
+    methods:{
+        createCards(){
+            axios
+                .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+                .then((res)=>{
+                    console.log(res)
+                })
+        }
+    },
+    created(){
+        this.createCards()
+    }
+  }
+</script>
+
+<template>
+   
+    <ul>
+        <li>
+            carta
+        </li>
+    </ul>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
