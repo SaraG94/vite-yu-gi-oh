cedd92d75b0ca5729def3770080808b4a4f06efa
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-06 17:23:42 +0100

    FEAT api filter

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -26,7 +26,14 @@
                 console.log(search,'=valore search')
 
                 axios
-                    .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+                    //.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0&fname=${search}`) prima soluzione
+                    .get('https://db.ygoprodeck.com/api/v7/cardinfo.php',{
+                        params:{
+                            fname:search,
+                            num:20,
+                            offset:0
+                        }
+                    })
                     .then((res)=>{
                         console.log(res.data.data);
                         this.store.cards = res.data.data
